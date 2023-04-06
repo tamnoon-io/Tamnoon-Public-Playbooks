df7f3531891c73ac69262f68e4ef8814dc034fd0
idan <idan@tamnoon.io>
2023-04-06 11:35:30 -0700

    Improve the VPC create_flow_logs automation to support all regions within one execution disclamer - doesn't support new regions yet

--- EC2Actions/EC2Helper.py
+++ EC2Actions/EC2Helper.py
@@ -38,21 +38,33 @@ def print_help():
         '\n'
         '\t\t\t Dependencies:\n'
         '\t\t\t\t \n'
+        '\t\t\t Authentication:\n'
+        '\t\t\t\t The script support the fallback mechanism auth as AWS CLI\n'
+        '\t\t\t\t\t profile - send the aws profile as input parameter\n'
+        '\t\t\t\t\t key and secret - send the aws key and secret as input parameter\n'
+        
+        
         '\t\t\t Supported Actions:\n'
         '\t\t\t\t 1. Snapshot - \n'
         '\t\t\t\t\t\t delete, ls\n'
+        '\t\t\t\t\t\t\t example python3 EC2Helper.py  --type snapshot --action delete --assetIds "snap-1,snap-2" --dryRun True\n'
+        '\t\t\t\t\t\t\t example python3 EC2Helper.py  --type snapshot --action ls\n'
         '\t\t\t\t\t\t encrypt\n'
         '\t\t\t\t\t\t\t actionParams:\n'
         '\t\t\t\t\t\t\t\t KmsKeyId (OPTIONAL) \n'
         '\t\t\t\t\t\t\t\t\t The kms key to use for encryption, If this parameter is not specified, your KMS key for Amazon EBS is used\n'
+        '\t\t\t\t\t\t\t\t\t\t example python3 EC2Helper.py  --type snapshot --action encrypt --assetIds "snap-1,snap-2" --actionParams "{\\"KmsKeyId\\":\\"id\\"}"\n'
         '\t\t\t\t 2. SecurityGroup - delete  \n'
+        '\t\t\t\t\t\t example python3 EC2Helper.py  --type security-group --action delete --assetIds "securityGroup1"\n'
         '\t\t\t\t 3. Vpc - \n'
         '\t\t\t\t\t\t create_flow_log\n'
         '\t\t\t\t\t\t\t actionParams:\n'
         '\t\t\t\t\t\t\t\t DeliverLogsPermissionArn (REQUIRED)\n'
         '\t\t\t\t\t\t\t\t\t The ARN of the IAM role that allows Amazon EC2 to publish flow logs to a CloudWatch Logs log group in your account. \n'
         '\t\t\t\t\t\t\t\t LogGroupName (OPTIONAL)\n'
-        '\t\t\t\t\t\t\t\t\t The name of a new or existing CloudWatch Logs log group where Amazon EC2 publishes your flow logs. \n'
+        '\t\t\t\t\t\t\t\t\t The name of a new or existing CloudWatch Logs log group where Amazon EC2 publishes your flow logs.\n'
+        '\t\t\t\t\t\t\t\t\t\t example python3 EC2Helper.py  --type vpc --action create_flow_log --assetIds "all" --regions "all"\n'
+        
 
 
         '\n'
@@ -66,6 +78,9 @@ def print_help():
         '\t\t\t Parameter Usage:\n'
         '\t\t\t\t logLevel - The logging level (optional). Default = Info\n'
         '\t\t\t\t profile -  The AWS profile to use to execute this script\n'
+        '\t\t\t\t awsAccessKey -  The AWS access key to use to execute this script\n'
+        '\t\t\t\t awsSecret -  The AWS secret to use to execute this script\n'
+        '\t\t\t\t region -   The AWS region to use to execute this script\n'
         '\t\t\t\t type -     The AWS EC2 asset type - for example - instance,snapshot,security-group ....\n'
         '\t\t\t\t action -   The EC2 action to execute - (snapshot-delete, sg-delete)\n'
         '\t\t\t\t actionParmas (optional)  - A key value Dictionary of action params. each " should be \\" for exampel {\\"key1\\":\\"val1\\"}\n'
@@ -76,11 +91,25 @@ def print_help():
     print(text)
 
 
-def setup_session(profile):
+def setup_session(profile=None, region=None, aws_access_key=None, aws_secret=None):
+    '''
+    This method setup the boto session to AWS
+    :param profile:  The aws credentials profile as they defined on the machine (~/.aws/credentials)
+    :param region:   The aws target region to execute on
+    :param aws_access_key:
+    :param aws_secret:
+    :return:
+    '''
     if profile:
-        session = boto3.Session(profile_name=profile)
-        return session
-
+        if region:
+            return boto3.Session(profile_name=profile, region_name=region)
+        return boto3.Session(profile_name=profile)
+    if aws_access_key and aws_secret:
+        if region:
+            return boto3.Session(region_name=region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret)
+        return boto3.Session(aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret)
+    if region:
+        return boto3.Session(region_name=region)
     return boto3.Session()
 
 
@@ -208,13 +237,58 @@ def do_sg_action(session, dry_run, action, asset_ids):
             do_sg_delete(resource=resource, asset_id=asset_id, dry_run=dry_run)
 
 
+def _get_regions(regions_param, session):
+    """
+    This method extract thje list of regions to run over
+    :param regions_param: The input region parameter - could be single region, multi regions (reg1,reg2...) or 'all'
+    :param session: boto3 ec2 session
+    :return:
+    """
+    regions_list = list()
+    if 'all' in regions_param:
+        ec2_client = session.client('ec2')
+        response = ec2_client.describe_regions(AllRegions=True)
+        for region in response['Regions']:
+            regions_list.append(region['RegionName'])
+        return regions_list
+
+    return regions_param.split(",")
+
+
+def _get_vpcs_in_region(session):
+    """
+    This method return all the vpc ids inside the region
+    :param session:
+    :return:
+    """
+    try:
+        ec2_client = session.client('ec2')
+        vpc_ids = list()
+        response = ec2_client.describe_vpcs()
+        vpcs  = response['Vpcs']
+        while 'NextToken' in response:
+            response = ec2_client.describe_vpcs(NextToken=response['NextToken'])
+            vpcs = vpcs + response['Vpcs']
+
+        for vpc in vpcs:
+            vpc_ids.append(vpc['VpcId'])
+
+        return vpc_ids
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'AuthFailure':
+            logging.warning(f"The region {session.region_name} doesn't support this action please do it via console")
+            return list()
+
+
+
+
 def do_vpc_action(session, dry_run, action, asset_ids, parmas=None):
     """
     This i the function that handle the vpc actions
-    :param session:
+    :param session: The boto3 session
     :param dry_run:
-    :param action:
-    :param asset_ids:
+    :param action: The action to execute
+    :param asset_ids: the list of asset ids or 'all'
     :param action_parmas:
     :return:
     """
@@ -228,15 +302,27 @@ def do_vpc_action(session, dry_run, action, asset_ids, parmas=None):
 
     deliver_logs_permission_arn = parmas['DeliverLogsPermissionArn']
     if action == 'create_flow_log':
-        for asset_id in asset_ids:
-            if not 'LogGroupName' in parmas:
-                log_group_name = f"Vpc_FlowLog_{asset_id}"
-            else:
-                log_group_name = parmas['LogGroupName']
-
-            logging.info(f"Going to execute - {action} for asset type - {asset_type} asset - {asset_id}")
-            do_create_flow_log(session=session, asset_id=asset_id, dry_run=dry_run, log_group_name=log_group_name,
+        # check regions
+        if len(asset_ids) == 1 and asset_ids[0] == 'all':
+            list_of_vpcs = _get_vpcs_in_region(session)
+            for asset_id in list_of_vpcs:
+                if not 'LogGroupName' in parmas:
+                    log_group_name = f"Vpc_FlowLog_{asset_id}"
+                else:
+                    log_group_name = parmas['LogGroupName']
+                do_create_flow_log(session=session, asset_id=asset_id, dry_run=dry_run,
+                               log_group_name=log_group_name,
                                deliver_logs_permission_arn=deliver_logs_permission_arn)
+        else:
+            for asset_id in asset_ids:
+                if not 'LogGroupName' in parmas:
+                    log_group_name = f"Vpc_FlowLog_{asset_id}"
+                else:
+                    log_group_name = parmas['LogGroupName']
+
+                logging.info(f"Going to execute - {action} for asset type - {asset_type} asset - {asset_id}")
+                do_create_flow_log(session=session, asset_id=asset_id, dry_run=dry_run, log_group_name=log_group_name,
+                                   deliver_logs_permission_arn=deliver_logs_permission_arn)
 
 
 def do_create_flow_log(session, dry_run, asset_id, log_group_name=None, deliver_logs_permission_arn=None):
@@ -252,7 +338,7 @@ def do_create_flow_log(session, dry_run, asset_id, log_group_name=None, deliver_
     )
 
     if len(describe_response['FlowLogs']) > 0:
-        logging.info(f"No Need to create a vpc flow log for vpc - {asset_id}, it's already exists")
+        logging.info(f"No Need to create a vpc flow log for vpc - {asset_id} at region {session.region_name}, it's already exists")
         return
 
     response = ec2_client.create_flow_logs(
@@ -264,6 +350,15 @@ def do_create_flow_log(session, dry_run, asset_id, log_group_name=None, deliver_
         LogDestinationType='cloud-watch-logs')
 
 
+def _do_action(asset_type, session, dry_run, action, asset_ids, action_parmas):
+    if asset_type == 'snapshot':
+        do_snapshot_action(session=session, dry_run=dry_run, action=action, asset_ids=asset_ids, action_parmas=params)
+    if asset_type == 'security-group':
+        do_sg_action(session=session, dry_run=dry_run, action=action, asset_ids=asset_ids)
+    if asset_type == 'vpc':
+        do_vpc_action(session=session, dry_run=dry_run, action=action, asset_ids=asset_ids, parmas=params)
+
+
 if __name__ == '__main__':
 
     # TODO - Work on desc for params
@@ -272,6 +367,9 @@ if __name__ == '__main__':
     parser.add_argument('--profile', required=False, default=None)
     parser.add_argument('--type', required=True, type=str)
     parser.add_argument('--action', required=True, type=str)
+    parser.add_argument('--regions', required=False, type=str, default=None)
+    parser.add_argument('--awsAccessKey', required=False, type=str, default=None)
+    parser.add_argument('--awsSecret', required=False, type=str, default=None)
     parser.add_argument('--assetIds', required=True, type=str)
     parser.add_argument('--actionParams', required=False, type=json.loads, default=None)
     parser.add_argument('--dryRun', required=False, type=bool, default=False)
@@ -293,13 +391,20 @@ if __name__ == '__main__':
     params = args.actionParams
     dry_run = args.dryRun
     asset_type = args.type
+    regions = args.regions
+    aws_access_key = args.awsAccessKey
+    aws_secret = args.awsSecret
 
     logging.info("Going to setup resource")
-    session = setup_session(profile)
 
-    if asset_type == 'snapshot':
-        do_snapshot_action(session=session, dry_run=dry_run, action=action, asset_ids=asset_ids, action_parmas=params)
-    if asset_type == 'security-group':
-        do_sg_action(session=session, dry_run=dry_run, action=action, asset_ids=asset_ids)
-    if asset_type == 'vpc':
-        do_vpc_action(session=session, dry_run=dry_run, action=action, asset_ids=asset_ids, parmas=params)
+    if regions:
+        # in case that regions parameter is set , assume that we want to enable all vpc flow logs inside the region
+        session = setup_session(profile=profile, aws_access_key=aws_access_key, aws_secret=aws_secret)
+        list_of_regions = _get_regions(regions_param=regions, session=session)
+        for region in list_of_regions:
+            session = setup_session(profile=profile, region=region, aws_access_key=aws_access_key, aws_secret=aws_secret)
+            _do_action(asset_type=asset_type, session=session, dry_run=dry_run, action=action, asset_ids=asset_ids, action_parmas=params)
+    else:
+        session = setup_session(profile=profile, aws_access_key=aws_access_key, aws_secret=aws_secret)
+        _do_action(asset_type=asset_type, session=session, dry_run=dry_run, action=action, asset_ids=asset_ids,
+                   action_parmas=params)
--- EC2Actions/README.md
+++ EC2Actions/README.md
@@ -31,8 +31,7 @@ After authentication via AWS API, the script execution will run on the same AWS 
 
 
 ## Script help page 
-
-                     ___                                                                                           
+			 ___                                                                                           
 			(   )                                                                            .-.           
 			 | |_       .---.   ___ .-. .-.    ___ .-.     .--.     .--.    ___ .-.         ( __)   .--.   
 			(   __)    / .-, \ (   )   '   \  (   )   \   /    \   /    \  (   )   \        (''")  /    \  
@@ -48,21 +47,30 @@ After authentication via AWS API, the script execution will run on the same AWS 
 
 			 Dependencies:
 				 
+			 Authentication:
+				 The script support the fallback mechanism auth as AWS CLI
+					 profile - send the aws profile as input parameter
+					 key and secret - send the aws key and secret as input parameter
 			 Supported Actions:
 				 1. Snapshot - 
 						 delete, ls
+							 example python3 EC2Helper.py  --type snapshot --action delete --assetIds "snap-1,snap-2" --dryRun True
+							 example python3 EC2Helper.py  --type snapshot --action ls
 						 encrypt
 							 actionParams:
 								 KmsKeyId (OPTIONAL) 
 									 The kms key to use for encryption, If this parameter is not specified, your KMS key for Amazon EBS is used
+										 example python3 EC2Helper.py  --type snapshot --action encrypt --assetIds "snap-1,snap-2" --actionParams "{\"KmsKeyId\":\"id\"}"
 				 2. SecurityGroup - delete  
+						 example python3 EC2Helper.py  --type security-group --action delete --assetIds "securityGroup1"
 				 3. Vpc - 
 						 create_flow_log
 							 actionParams:
 								 DeliverLogsPermissionArn (REQUIRED)
 									 The ARN of the IAM role that allows Amazon EC2 to publish flow logs to a CloudWatch Logs log group in your account. 
 								 LogGroupName (OPTIONAL)
-									 The name of a new or existing CloudWatch Logs log group where Amazon EC2 publishes your flow logs. 
+									 The name of a new or existing CloudWatch Logs log group where Amazon EC2 publishes your flow logs.
+										 example python3 EC2Helper.py  --type vpc --action create_flow_log --assetIds "all" --regions "all"
 
 				 The script is based on AWS API and documentation 
 				 https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
@@ -76,7 +84,10 @@ After authentication via AWS API, the script execution will run on the same AWS 
 			 Parameter Usage:
 				 logLevel - The logging level (optional). Default = Info
 				 profile -  The AWS profile to use to execute this script
+				 awsAccessKey -  The AWS access key to use to execute this script
+				 awsSecret -  The AWS secret to use to execute this script
+				 region -   The AWS region to use to execute this script
 				 type -     The AWS EC2 asset type - for example - instance,snapshot,security-group ....
 				 action -   The EC2 action to execute - (snapshot-delete, sg-delete)
-				 actionParmas (optional)  - A key value Dictionary of action params. each " should be \" for exampel {\"key1\":\"val1\"} 
+				 actionParmas (optional)  - A key value Dictionary of action params. each " should be \" for exampel {\"key1\":\"val1\"}
 				 assetIds  - List of assets ids (string seperated by commas)"
